25df691161c195d82a62c5765c41d74a39f3f5a6
45h1m <ady.ashim@gmail.com>
2024-07-09 17:35:37 +0530

    cleanup home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,59 +6,6 @@ import PostComment from "@/components/PostComment";
 export default async function Home() {
     return (
         <div className="min-h-svh pt-20">
-            <section>
-                <div className="max-w-lg mx-auto">
-                    <PostComment/>
-                    <h3 className="text-lg text-slate-400">Comments</h3>
-
-
-                    <ul className="pt-5 flex flex-col gap-4">
-                        <li>
-                            <CommentCard
-                                author={"Ashim"}
-                                authorDP={""}
-                                date="12/12/2030"
-                                authenticated={true}
-                                owner={true}
-                                comment={
-                                    
-                                    ''
-                                }
-                            />
-                        </li>
-                        <li>
-                            <CommentCard
-                                author={"Ashim"}
-                                authorDP={"/admin-dp-small.gif"}
-                                date="12/12/2030"
-                                authenticated={true}
-                                owner={true}
-                                comment={
-                                    
-                                    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur architecto est porro quasi doloremque aut quisquam vero necessitatibus distinctio dignissimos?"
-                                }
-                            />
-                        </li>
-                        <li>
-                            <CommentCard
-                                author={"Ashim"}
-                                authorDP={"/admin-dp-small.gif"}
-                                date="12/12/2030"
-                                authenticated={true}
-                                owner={true}
-                                comment={
-                                    
-                                    "Lorem, ipsum est porro quasi doloremque aut quisquam vero necessitatibus distinctio dignissimos?"
-                                }
-                            />
-                        </li>
-                    </ul>
-                </div>
-            </section>
-
-            <section>
-                
-            </section>
 
             <div className="pt-4 flex flex-col gap-3 items-center">
                 <Image width={300} height={250} src="/building.gif" alt="webpage-is-building-animation" className="w-full max-w-sm rounded-lg" />
@@ -69,7 +16,7 @@ export default async function Home() {
                 <h5 className="text-xl font-bold pt-5">still building this site 😉</h5>
                 <a
                     href="/blog"
-                    className="flex w-fit items-center gap-2 px-3 py-1 bg-red-500 hover:bg-red-400 rounded-sm text-white font-semibold mt-2"
+                    className="flex w-fit items-center gap-2 px-3 py-1 bg-red-600 hover:bg-red-400 rounded-sm text-white font-semibold mt-2"
                 >
                     View Blogs{" "}
                     <span className="pt-1">
