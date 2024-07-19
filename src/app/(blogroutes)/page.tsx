75287711ed7994dcb6e6a3c23978e49cd381301f
45h1m <ady.ashim@gmail.com>
2024-07-19 09:16:22 +0530

    added new story

--- src/app/(blogroutes)/page.tsx
+++ src/app/(blogroutes)/page.tsx
@@ -17,7 +17,47 @@ export async function generateMetadata(props: any): Promise<Metadata> {
 }
 
 export default async function Home() {
+    // const newStory: StoryType = {
+    //     title: "Track Realtime BPM & Blood Oxygen Level 🧐",
+    //     author: "Flamer",
+    //     authorDP: "/flamer.png",
+    //     cover: {
+    //         image: "assets/images/oxilive/oxi-s-cover.webp",
+    //         audio: null,
+    //     },
+    //     slug: "bpm-oxygen-monitoring-oxilive",
+    //     pages: [
+    //         {
+    //             image: "assets/images/oxilive/oxi-s-live.webp",
+    //             title: "Realtime Updates",
+    //             description: "A pulse oximeter measures your blood oxygen levels and pulse. A low level of oxygen saturation may occur if you have certain health conditions.",
+    //             audio: null,
+    //             video: null,
+    //             href: "/blog/oxilive-oxygen-bpm-app",
+    //         },
+    //         {
+    //             image: "assets/images/oxilive/oxi-s-graph.webp",
+    //             title: "Interactive Bar Graph",
+    //             description: "Monitoring your BPM and blood-oxygen levels throughout the day can help you identify patterns and make informed decisions to improve your physical and mental well-being.",
+    //             audio: null,
+    //             video: null,
+    //             href: "/blog/oxilive-oxygen-bpm-app",
+    //         },
+    //         {
+    //             image: "assets/images/oxilive/oxi-s-history.webp",
+    //             title: "Records History Per Day Basis",
+    //             description: "Only logged-in user can use this app. App uses JWT for authentication & authorization. User can log in with simple OTP to from email.",
+    //             audio: null,
+    //             video: null,
+    //             href: "/blog/oxilive-oxygen-bpm-app",
+    //         },
+    //     ],
+    //     nexthref: "/blog/oxilive-oxygen-bpm-app",
+    // };
 
+    // const res = await postStory(newStory);
+
+    // console.log(res);
 
     return (
         <main className="">
